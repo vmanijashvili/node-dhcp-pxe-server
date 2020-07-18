1e56e3b974f1972f34adf8b7caf3eab0aab73486
valeri <info@ini.ge>
2020-07-18 23:32:01 +0400

    example

--- example/example.ts
+++ example/example.ts
@@ -1,17 +1,13 @@
 
 import {PxeServer} from "../src/lib/pxe-server";
 import {DhcpMessage} from "../src/interfaces/dhcp-message";
-import fs from "fs";
 
 let server = new PxeServer();
 server.on("boot", (message: DhcpMessage, callback) => {
-	//console.log("Receive Message: ", message);
 	callback({
 		directory: "./tftp",
 		file: "undionly.kpxe",
 		loader: "boot.ipxe"
 	});
-	//callback("undionly.kpxe", fs.statSync("/OpenTFTPServer/undionly.kpxe").size);
 });
 server.bind();
-//*/
